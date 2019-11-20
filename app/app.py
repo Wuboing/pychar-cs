88db16281e9d2b4e45af0c77b70f4ec071c168ab
Wuboing <2034045894@qq.com>
2019-11-20 10:15:46 +0800

    flask测试1

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for, request, render_template, abort, flash
+
+app = Flask(__name__,)
+
+
+app.secret_key = 'random string'
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+
+
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+            request.form['password'] != '123456':
+            error = 'Invalid username or password. Please try again!'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+
+
+    return render_template('login.html', error = error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
