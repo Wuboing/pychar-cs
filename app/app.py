ba1f4990eafeb76609cf1944c2a259a9bd3e4433
Wuboing <2034045894@qq.com>
2019-11-22 13:55:44 +0800

    添加返回数据

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template, abort, flash
+from flask import Flask, redirect, url_for, request, render_template, abort, flash, jsonify
 
 app = Flask(__name__,)
 
@@ -24,6 +24,32 @@ def login():
 
 
     return render_template('login.html', error = error)
+
+@app.route('/api/choose', methods = ['GET', 'POST'])
+def choose():
+    return '[{"id":"1","date":"2016-10-18","name":"章邯","address":"上海市普陀区"},{"id":"2","date":"2017-05-04","name":"李四","address":"上海市杨浦区"},{"id":"3","date":"2017-08-16","name":"张三","address":"上海市虹口区"},{"id":"4","date":"2018-05-03","name":"王二小","address":"北京康复路58号"}]'
+
+
+@app.route('/api/menu/user/menus', methods = ['POST'])
+def menus():
+    return 'this is meuns'
+
+
+# 后端接受参数
+# @app.route('/api/delete', methods = ['POST'])
+# def deleteTable():
+#     data =request.form['data']
+#     if data == 0:
+#         return '{"id":"2","date":"2017-05-04","name":"李四","address":"上海市杨浦区"},{"id":"3","date":"2017-08-16","name":"张三","address":"上海市虹口区"},{"id":"4","date":"2018-05-03","name":"王二小","address":"北京康复路58号"}]'
+#     elif data == 1:
+#         return '[{"id":"1","date":"2016-10-18","name":"章邯","address":"上海市普陀区"},{"id":"3","date":"2017-08-16","name":"张三","address":"上海市虹口区"},{"id":"4","date":"2018-05-03","name":"王二小","address":"北京康复路58号"}]'
+#     elif data == 2:
+#         return '[{"id":"1","date":"2016-10-18","name":"章邯","address":"上海市普陀区"},{"id":"2","date":"2017-05-04","name":"李四","address":"上海市杨浦区"},{"id":"4","date":"2018-05-03","name":"王二小","address":"北京康复路58号"}]'
+#     elif data == 3:
+#         return '[{"id":"1","date":"2016-10-18","name":"章邯","address":"上海市普陀区"},{"id":"2","date":"2017-05-04","name":"李四","address":"上海市杨浦区"},{"id":"3","date":"2017-08-16","name":"张三","address":"上海市虹口区"}]'
+#
+
+
 
 
 if __name__ == '__main__':
